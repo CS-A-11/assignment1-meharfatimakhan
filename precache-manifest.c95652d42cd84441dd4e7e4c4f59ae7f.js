8e2bfdcf78330981a4869623e086240ceefa86b4
Mehar Fatima Khan <meharfatimak@gmail.com>
2020-07-23 21:03:42 +0500

    Updates

---
+++ precache-manifest.c95652d42cd84441dd4e7e4c4f59ae7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6269a004e3a31d77d91be381d0a2f139",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "839fd941bedc285aee68",
+    "url": "/monsters-rolodex/static/css/main.3c2104ac.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839fd941bedc285aee68",
+    "url": "/monsters-rolodex/static/js/main.ed6c0ff7.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
